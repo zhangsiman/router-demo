116f7c6433dea0996c39a4283e56f3d85595494c
zhangsiman <1009687043@qq.com>
2016-12-01 16:25:11 +0800

    first

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Routers from './Routers.js';
-import './Main.css';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
-render(<Routers />,document.getElementById('root'));
