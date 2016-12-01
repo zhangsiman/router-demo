8ae5e6ca06fb2e48e82cd156b59114850b90e59f
zhangsiman <1009687043@qq.com>
2016-12-01 16:08:44 +0800

    header

--- src/component/NavHeader.js
+++ src/component/NavHeader.js
@@ -2,23 +2,40 @@ import React, { PropTypes } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import Img1 from '../image/a.jpg'
 
 
 
 class NavHeader extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      show:true
+    }
+  }
   handleClick(){
     this.context.router.push('/');
   }
+  handleMa(){
+      this.setState({
+        show:!this.state.show
+      })
+  }
   render () {
+    let styles={
+          span:{fontSize:'20px',cursor:'pointer'}
+    }
       return(
         <div className="nav-header">
-          <button type="button" className="btn btn-default btn-sm" onClick={this.handleClick.bind(this)}>
-            <span className="glyphicon glyphicon-chevron-left"></span> Back
-          </button>
+          <div>
+              <span className="glyphicon glyphicon-th-list" onClick={this.handleClick.bind(this)} style={styles.span}></span>
+          </div>
               <h3>SiMan@{this.props.title}</h3>
-          <button type="button" className="btn btn-default btn-lg">
-            <span className="glyphicon glyphicon-camera"></span>
-          </button>
+            <div>
+              <span className="glyphicon glyphicon-barcode"  style={styles.span}></span>
+
+            </div>
+
         </div>
       )
   }
