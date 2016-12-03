e56a8f2f8e89469263a3f126200a7e4463d0ca07
zhangsiman <1009687043@qq.com>
2016-12-03 16:30:26 +0800

    item

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,15 +1,25 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import marked from "marked";
+import axios from "axios";
+import Loading from "../component/Loading"
 
-var marked = require('marked');
 class Item extends React.Component {
-  render () {
-    let content=this.props.params.title==0? "这是第一个页面"　:
-                this.props.params.title==1 ? "这是第二个页面"　:
-                this.props.params.title==2 ? "这是第三个页面"  : "这是第n个页面"
+      constructor(){
+        super();
+        this.state={
+          data:''
+        }
+      }
+    componentDidMount(){
+      let address=this.props.params.title;
+      axios.get(`https://raw.githubusercontent.com/zhangsiman/router-demo/master/data/${address}.md`)
+            .then(res=>this.setState({data:res.data}))
+    }
+    render () {
     return(
-      <div>
-        {content}
-        <div dangerouslySetInnerHTML={{__html:marked('`个人博客`')}}></div>
+      <div className='item-wrap'>
+        {this.state.data.length==0 ? <Loading /> :
+        <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>}
       </div>
     )
 
